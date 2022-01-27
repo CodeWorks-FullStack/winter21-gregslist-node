ec6d13c305522c078def88ceb07abed26c64dd6b
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2022-01-27 09:23:14 -0700

    sync

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
 import { CarSchema } from '../models/Car'
-<<<<<<< HEAD
-import { CarCommentSchema } from '../models/CarComment'
-=======
 import { CarBidSchema } from '../models/CarBid'
->>>>>>> bb62acc0220f0e532f4ce431f4a39137a04b114b
+import { CarCommentSchema } from '../models/CarComment'
 import { ValueSchema } from '../models/Value'
 
 // NOTE this is where we want to register our Schemas, so that we can reference them when we populate data, or access the database
@@ -15,11 +12,8 @@ class DbContext {
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
   Cars = mongoose.model('Car', CarSchema)
-<<<<<<< HEAD
   CarComments = mongoose.model('CarComment', CarCommentSchema)
-=======
   CarBids = mongoose.model('CarBid', CarBidSchema)
->>>>>>> bb62acc0220f0e532f4ce431f4a39137a04b114b
 }
 
 export const dbContext = new DbContext()
